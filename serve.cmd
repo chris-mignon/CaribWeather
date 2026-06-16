d47583329a0c35178b9a18b8bdbb7d4f54bd5074
christopher mignon <dspmosfet@gmail.com>
2026-06-16 18:31:56 -0400

    Create serve.cmd

---
+++ serve.cmd
@@ -0,0 +1,8 @@
+@echo off
+setlocal
+
+for /f "tokens=2,*" %%A in ('reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path 2^>nul') do set "MACHINE_PATH=%%B"
+for /f "tokens=2,*" %%A in ('reg query "HKCU\Environment" /v Path 2^>nul') do set "USER_PATH=%%B"
+set "PATH=%MACHINE_PATH%;%USER_PATH%"
+
+php artisan serve --host=127.0.0.1 --port=8000
